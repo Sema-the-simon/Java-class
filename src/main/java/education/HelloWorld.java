e412d493501cac47dc40e577bc84476dc815be93
Sema Buglaev <buglaev.sa@edu.spbstu.ru>
2021-02-04 18:16:33 +0700

    start working

---
+++ src/main/java/education/HelloWorld.java
@@ -0,0 +1,7 @@
+package education;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("HelloWorld i'm first java program of Sema-the-Simon");
+    }
+}
