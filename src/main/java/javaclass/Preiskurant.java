5b5cd449f29178eb8658476d331412308c65495b
Sema Buglaev <buglaev.sa@edu.spbstu.ru>
2021-02-13 13:09:00 +0300

    class created ProuctTest created

--- src/main/java/javaclass/Preiskurant.java
+++ src/main/java/javaclass/Preiskurant.java
@@ -1,11 +1,9 @@
 package javaclass;
 
 import java.util.*;
-import java.util.Map.Entry;
-import java.util.AbstractMap.SimpleEntry;
 
 public final class Preiskurant {
-    private final List<Entry<Product, Price>> pricelist = new ArrayList<>();
+    private final List<Pair<Product, Price>> pricelist = new ArrayList<>();
 
     private static final class Price {
         private final int rub;
@@ -44,19 +42,64 @@ public final class Preiskurant {
         }
     }
 
-    public Entry<Product, Price> createEntry(Product product, Price price) {
-        return new AbstractMap.SimpleEntry<>(product, price);
+    public static class Pair<K, V> {
+
+        private K key;
+        private V value;
+
+        public Pair(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+
+        public void setKey(K key) {
+            this.key = key;
+        }
+
+        public void setValue(V value) {
+            this.value = value;
+        }
+
+        @Override
+        public int hashCode() {
+            return key.hashCode() ^ value.hashCode();
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (!(o instanceof Pair)) return false;
+            Pair<?, ?> pairo = (Pair<?, ?>) o;
+            return this.key.equals(pairo.getKey()) &&
+                    this.value.equals(pairo.getValue());
+        }
+
+        @Override
+        public String toString() {
+            return getKey() + " to " + getValue();
+        }
+    }
+
+    public Pair<Product, Price> createPair(Product product, Price price) {
+        return new Pair<>(product, price);
     }
 
     public Boolean addProductAndPrice(Product product, Price price) {
         if (product == null) return false;
-        pricelist.add(createEntry(product, price));
+        pricelist.add(createPair(product, price));
         return true;
     }
 
     public boolean priceChange(Product product, Price newprice) {
         if (!containsProduct(product)) return false;
-        for (Entry pair : pricelist) {
+        for (Pair<Product, Price> pair : pricelist) {
             if (pair.getKey() == product) {
                 pair.setValue(newprice);
             }
@@ -64,24 +107,82 @@ public final class Preiskurant {
         return true;
     }
 
-    public boolean nameChange(Product product, Price newprice) {
-        if (!containsProduct(product)) return false;
-        for (Entry pair : pricelist) {
-            if (pair.getKey() == product) {
+    public boolean priceChange(int code, Price newprice) {
+        if (!containsProduct(code)) return false;
+        for (Pair<Product, Price> pair : pricelist) {
+            if (pair.getKey().getCode() == code) {
                 pair.setValue(newprice);
             }
         }
         return true;
     }
-    public boolean delite(Product product) {
-        if (!pricelist.contains(product)) return false;
-        pricelist.remove(product);
+
+    public boolean nameChange(Product product, String newname) {
+        if (!containsProduct(product)) return false;
+        for (Pair<Product, Price> pair : pricelist) {
+            if (pair.getKey() == product) {
+                pair.getKey().setName(newname);
+            }
+        }
         return true;
+    }
+
+    public boolean delete(Product product) {
+        if (!containsProduct(product)) return false;
+        for (Pair<Product, Price> pair : pricelist) {
+            if (pair.getKey() == product) {
+                pricelist.remove(pair);
+                break;
+            }
+        }
+        return true;
+    }
+
+    public boolean deleteAll(Product product) {
+        if (!containsProduct(product)) return false;
+        pricelist.removeIf(pair -> pair.getKey() == product);
+        return true;
+    }
+
+    public boolean delete(int code) {
+        if (!containsProduct(code)) return false;
+        for (Pair<Product, Price> pair : pricelist) {
+            if (pair.getKey().getCode() == code) {
+                pricelist.remove(pair);
+                break;
+            }
+        }
+        return true;
+    }
+
+    public boolean deleteAll(int code) {
+        if (!containsProduct(code)) return false;
+        pricelist.removeIf(pair -> pair.getKey().getCode() == code);
+        return true;
+    }
+
+    public String purchasePrice(int code) {
+        int kop = 0;
+        if (!containsProduct(code)) throw  new IllegalArgumentException("This preiskurant does not contain Product:" + code);
+        for (Pair<Product, Price> pair : pricelist) {
+            if (pair.getKey().getCode() == code) {
+                kop += pair.getValue().getRub() * 100 ;
+                kop += pair.getValue().getKop() ;
+            }
+        }
+        return new Price(kop).toString();
     }
 
     public boolean containsProduct(Product product) {
-        for (Entry pair : pricelist) {
+        for (Pair<Product, Price> pair : pricelist) {
             if (pair.getKey() == product) return true;
+        }
+        return false;
+    }
+
+    public boolean containsProduct(int code) {
+        for (Pair<Product, Price> pair : pricelist) {
+            if (pair.getKey().getCode() == code) return true;
         }
         return false;
     }
